4acf282305f12c941e0f00559d797e861aab32b1
joannaare15 <joannaare15>
2024-10-29 11:24:46 -0600

    Plantilla Agregar

--- api/home/home_view.py
+++ api/home/home_view.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(reques):
-    template_name = "login.html"
-    
+    template_name = "index.html"
+
     return render(reques,template_name)
